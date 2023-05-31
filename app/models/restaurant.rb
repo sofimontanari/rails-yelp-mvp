bce9ff6fb73400e73282f6ade04bf17c373bfaf2
Sofia Montanari <sofiamontanari.a@gmail.com>
2023-05-31 17:12:32 -0300

    mejora en estilo

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,6 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  CATEGORIES = %w(chinese italian japanese french belgian)
   validates :name, :address, presence: true
-  validates :category, inclusion: { in: %w(chinese italian japanese french belgian) }
+  validates :category, inclusion: { in: CATEGORIES }
 end
